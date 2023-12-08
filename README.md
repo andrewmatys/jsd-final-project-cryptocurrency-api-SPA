f63ec2d1c96ddb0764eabe60af9e211941e7bf6d
Andrew Matysiak <andrewmatysiak@hotmail.com>
2023-12-08 13:26:04 +1100

    testing and completion of coinChecker() and loadCoinDetails() functions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # jsd-final-project
-# My final project for General Assembly's JavasScript Development class. 
-# A single page app (SPA) utilising HTML/CSS/Javascript for the frontend and API endpoints to populate a cryptocurrency based webpage using various JSON data via AXIOS.
+# My final project for General Assembly's JavaScript Development class. 
+# A single page app (SPA) utilising HTML/CSS/JavaScript for the frontend and API endpoints to populate a cryptocurrency based webpage using various JSON data via AXIOS.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// LINES TO FIX: 42, 
+// coinmarketcap api_key = '7f07d733-d208-480d-856c-b4d39a6e3a82'
 
 const cryptoApp = {
 
@@ -14,7 +14,7 @@ const cryptoApp = {
     dom: {},
 
 
-    //coinExists: false,
+    coinExists: false,
 
 
     initUI() {
@@ -26,7 +26,7 @@ const cryptoApp = {
             //reviewsDiv: document.querySelector('#reviews'),
             topTenCoinsDiv: document.querySelector('#topTen'),
             coinDetails: document.querySelector('#details'),
-
+            topTenHeading: document.querySelector('#topTenHeading')
         };
     
 
@@ -37,10 +37,13 @@ const cryptoApp = {
             this.dom.topTenCoinsDiv.replaceChildren();                  // delete TOP 10 COIN LIST once form submitted
             //console.log(`Form submitted! ie BUTTON CLICKED`, Math.random());
             console.log('Value submitted:', this.dom.searchText.value); // user input
-            ev.preventDefault();                                        // prevent form submit page reload upon user submit button
-                                         
-            this.loadCoinDetails(this.coinChecker(this.dom.searchText.value));      // run loadCoinDetails function which runs an AJAX request with 'a validated coin' as argument. coinChecker returns a valid or ERR ...need to FIX THIS
-        });// addEventListener 'submit' event for submit form
+            const coinId = this.dom.searchText.value.toLowerCase();         // to suit 'id' key value
+            
+            ev.preventDefault();                                        // prevent form submit page reload upon user submit button ALLOW TO RELOAD
+        
+            this.coinChecker(coinId);
+        }); // addEventListener 'submit' event for submit form
+   
 
 
         this.dom.topTenCoinsDiv.addEventListener('click', (ev) => {
@@ -57,14 +60,13 @@ const cryptoApp = {
 
         axios.get(this.config.CRYPTO_BASE_URL, {
             params: {
-                api_key: this.config.API_KEY,
                 vs_currency: 'aud',             // currency type
                 order: 'market_cap_desc',       // list order by: type_descending/ascending
                 per_page: 10,                   // results per page
                 page: 1,                        // number of pages
-                sparkline: false,               // use data over last 7 days only
                 locale: 'en',                   // language
-                precision: 2                    // number of decimals
+                precision: 2,                   // number of decimals
+                api_key: this.config.API_KEY
             }
         })
         .then( (res) => {
@@ -88,92 +90,118 @@ const cryptoApp = {
             const pTag = document.createElement('p');
             pTag.innerHTML += (orderNumber + '. ' + coin.name + ': $' + coin.current_price);    // display coin and it's current price (top 10)
             pTag.dataset.id = coin.id;                                  // adds new attribute called "data-id" to the 'p' tag, which then stores the JSON key 'id' value of each coin. 'id' in this case is the name of the coin in lowerCase.
+                                                                        // this will allow ability to click on a coin in top 10 list instead of searching
+            
+            
             orderNumber++;                                              // increment top 10 counter by 1
             
             this.dom.topTenCoinsDiv.appendChild(pTag);                  // this.dom.topTenCoinsDiv is a DOM node we can append CHILD (pTag) to 
-
-
         }
 
     }, // renderTopTen())
 
-    
-    
-    coinChecker(inputData) { // this is necesary as a user may type 'BTC' or 'Bitcoin' or 'bitcoin' as searchText run through list 1st
+ 
+
+    coinChecker(inputData) {                               
+        console.log('arg passed to coinChecker():', inputData);
+
         axios.get(this.config.CRYPTO_FULL_LIST, {
             params: {
                 include_platform: false,        // include platform contract addresses
-                api_key: this.config.API_KEY,
                 vs_currency: 'aud',             // currency type
+                api_key: this.config.API_KEY
             }
         })
         .then( (res) => { 
-            if (inputData === res.data.id || inputData === res.data.symbol || inputData === res.data.name){
-                //this.coinExists = true;
-                //return res.data.id;                 // returns the coin's id value ie lowercase name regardless of whether Ticker symbol or name entered.
+            console.log('FULL list of coins: ', res.data);
+
+            for (const coin of res.data){                   // AXIOS get list successful, now do coin match and if that is successful: load data for coin)  
+                if (coin.id === inputData){        
+                    //console.log('COIN MATCHED!!!:', inputData);
+                    //console.log('coin.id KEY value: ', coin.id);
+                    this.coinExists = true;                 // set global variable to true
+                   
+                }
+            } // for loop 
+
+            if (this.coinExists) {     
+                //console.log('coinExists: status', this.coinExists);       //Check
                 axios.get(this.config.CRYPTO_BASE_URL, {
                     params: {
-                        api_key: this.config.API_KEY,
+                        ids: inputData,                 // coin name. Can use what the user entered or the coins id as have both ie 'inputData' OR 'coin.id'
                         vs_currency: 'aud',             // currency type
                         per_page: 1,
                         locale: 'en',                   // language
-                        precision: 2                    // number of decimals
+                        precision: 2,                   // number of decimals
+                        api_key: this.config.API_KEY
                     }
                 })
                 .then( (result) => { 
-                    console.log(`Coin found. Data following: `, result.data);  
-                    return result.data; //RETURN the 'result.data' back to calling function
+                    console.log(`Since Coin exists. Data following: `, result.data);       //WRONG DATA if empty string OR invalid coin id entered...
+                    this.loadCoinDetails(inputData); 
                 }) // .then()
                 .catch( (error) => {
-                   console.log(`Error loading data for the selected coin...`, error);
-                }) // .catch()
-            } // if statement coin check exists
+                console.log(`Error loading data for the selected coin...`, error);
+                }) // .catch()       
+            } else {
+                console.log('COIN DOES NOT EXIST! in list.');  
+                  // render to HTML //TODO   
+            }
         }) //.then()
         .catch( (err) => {
-           console.log(`This coin does not exist...`, err);
+           console.warn(`ERROR loading FULL list of coins`, err);
         }) //.catch()
     }, // coinChecker()
     
 
 
-////////////////////////////////////////////////////////ABOVE COMPLETE/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    loadCoinDetails(coinName){      
 
-//TODO:
+        console.log(`value submitted in form: `, coinName);                      // argument 'coinName' is value submitted in form by user that is verified as an existing coin by func coinchecker()
+        //https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&ids=bitcoin&precision=2
+        this.coinExists = false;                                // reset coinChecker()
+
+        axios.get(this.config.CRYPTO_BASE_URL, {
+            params: {
+                vs_currency: 'aud',              // currency type (input data)
+                ids: coinName,                  
+                precision: 2,                    // number of decimals
+                api_key: this.config.API_KEY
+            }
+        })
+        .then( (res) => {
+
+            console.log(`Coin details - Array of Objects:`, res.data);
+            console.log('details to be printed:', res.data[0].id, res.data[0].symbol, res.data[0].name, res.data[0].market_cap, res.data[0].price_change_24h, res.data[0].price_change_percentage_24h, res.data[0].ath);
+
+            this.renderCoinDetails(res.data[0].id) // pass selected movie object to func renderMovieDetails()            
+        }) //.then()
+        .catch( (err) => {
+            console.warn(`There was an error....`, err);
+        }) //.catch()
+
+    }, // loadMovieDetails()
+
+
+////////////////////////////////////////////////////////ABOVE COMPLETE//////////////////////////////////////////
+
+//TODO BELOW:
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// enter a coin and load results BELOW:
-
-
-loadCoinDetails(coinName){
-
-    console.log(`value submitted in form: `, coinName);                      // argument 'coinName' is value submitted in form by user (looking for id to match)
-    //https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&ids=bitcoin&per_page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2
-    axios.get(this.config.CRYPTO_BASE_URL, {
-        params: {
-            api_key: this.config.API_KEY,
-            vs_currency: 'aud',
-            ids: coinName,                  // currency type (input data)
-            per_page: 1,                    // results per page
-            locale: 'en',                   // language
-            precision: 2                    // number of decimals 
-        }
-    })
-    .then( (res) => {
-        console.log(`Coin details Array of Objects:`, res.data);
 
 
 
 
-        this.renderCoinDetails(res.data) // pass selected movie object to func renderMovieDetails()            
-    }) //.then()
-    .catch( (err) => {
-        console.warn(`There was an error....`, err);
-    }) //.catch()
-
-}, // loadMovieDetails()
 
 
 
-// renderCoinDetails(selectedCoin){};
+    renderCoinDetails(selectedCoin){
+        console.log('Coin details: ', selectedCoin); 
+    },// renderMovieDetails()
+
+
+
+
 
 
 
@@ -182,25 +210,53 @@ loadCoinDetails(coinName){
 cryptoApp.initUI(); ///initialises ie the methods in the variable don't run automatically
 cryptoApp.loadTopTen(); // run top 10 crypto
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 ISSUES/TODO:
-- when searching form input will have to be search against: 'data.id' and 'data.symbol' and 'data.name' to capture variations using params CREATE AN IF STATEMENT
-if not found then it will be caught can do ERR : coin does not exist
-
-- the search will also have to run again entire number of coins ie all pages to FIND or return NOT FOUND
-
-
-- use '/coins/list' to list all available coins supported
-
-
-
-- use 'BELOW' for coin details including link to IMG
-https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&ids=bitcoin&per_page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2
+============
+- RENDER MESSAGE TO USER coin does not exist in HTML (not just console.log)
 
 
 - images: render in top 10 list & for coinDetails page
+use 'BELOW' for coin details including link to IMG
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&ids=bitcoin&per_page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2
 
-- create a coin list if cannot find coin OR render on main page at start
+
+- make when click coin (top 10) CSS shadow dissappear??
+
+- hide 'top 10 coins by Marketcap' banner when submit form
+
+- remove unused CSS
+
+
+
+
+
+
+
+CHALLENGES FACED:
+=================
+- how i created coinChecker() show nested tree and for loop etc nneded to use coin id lowercase: - when searching, form input will have to be search against: 'data.id' not symbol as too ambiguous
+ie. issues many coins had the same name values in different keys eg 'symbol: "bitcoin", name: "ElonXAIDogeMessi69PepeInu" ' VS symbol: "btc", name: "Bitcoin"
+symbol key must be ignored therefore cant use coin codes as too ambiguous and  the value must be lowercase to suit 'id'
+
+
+
+
+
 
 
 */
